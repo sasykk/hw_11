eb6c5896e2219447a720eb89f5d271093182152d
stanislavsynyasvskyy <153124369+sasykk@users.noreply.github.com>
2024-06-16 15:09:14 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,49 @@
+from fastapi import FastAPI, Depends, HTTPException
+from sqlalchemy.orm import Session
+import crud
+import schemas
+from database import engine, Base, get_db
+
+Base.metadata.create_all(bind=engine)
+
+app = FastAPI()
+
+@app.post("/contacts/", response_model=schemas.Contact)
+def create_contact(contact: schemas.ContactCreate, db: Session = Depends(get_db)):
+    return crud.create_contact(db=db, contact=contact)
+
+@app.get("/contacts/", response_model=list[schemas.Contact])
+def read_contacts(skip: int = 0, limit: int = 10, db: Session = Depends(get_db)):
+    return crud.get_contacts(db, skip=skip, limit=limit)
+
+@app.get("/contacts/{contact_id}", response_model=schemas.Contact)
+def read_contact(contact_id: int, db: Session = Depends(get_db)):
+    db_contact = crud.get_contact(db, contact_id=contact_id)
+    if db_contact is None:
+        raise HTTPException(status_code=404, detail="Contact not found")
+    return db_contact
+
+@app.put("/contacts/{contact_id}", response_model=schemas.Contact)
+def update_contact(contact_id: int,
+                   contact: schemas.ContactUpdate,
+                   db: Session = Depends(get_db)):
+    db_contact = crud.update_contact(db, contact_id=contact_id, contact=contact)
+    if db_contact is None:
+        raise HTTPException(status_code=404, detail="Contact not found")
+    return db_contact
+
+@app.delete("/contacts/{contact_id}", response_model=schemas.Contact)
+def delete_contact(contact_id: int, db: Session = Depends(get_db)):
+    db_contact = crud.delete_contact(db, contact_id=contact_id)
+    if db_contact is None:
+        raise HTTPException(status_code=404, detail="Contact not found")
+    return db_contact
+
+@app.get("/contacts/search/", response_model=list[schemas.Contact])
+def search_contacts(query: str, db: Session = Depends(get_db)):
+    return crud.search_contacts(db, query=query)
+
+
+@app.get("/contacts/upcoming_birthdays/", response_model=list[schemas.Contact])
+def get_upcoming_birthdays(db: Session = Depends(get_db)):
+    return crud.get_upcoming_birthdays(db)

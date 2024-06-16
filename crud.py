dc35b9b8f054b6edb23b58d2f297ac0512d5a58a
stanislavsynyasvskyy <153124369+sasykk@users.noreply.github.com>
2024-06-16 15:07:38 +0300

    Create crud.py

---
+++ crud.py
@@ -0,0 +1,48 @@
+from sqlalchemy.orm import Session
+from models import Contact
+from schemas import ContactCreate, ContactUpdate
+from datetime import date, timedelta
+
+def get_contact(db: Session, contact_id: int):
+    return db.query(Contact).filter(Contact.id == contact_id).first()
+
+def get_contacts(db: Session, skip: int = 0, limit: int = 10):
+    return db.query(Contact).offset(skip).limit(limit).all()
+
+def create_contact(db: Session, contact: ContactCreate):
+    db_contact = Contact(**contact.dict())
+    db.add(db_contact)
+    db.commit()
+    db.refresh(db_contact)
+    return db_contact
+
+def update_contact(db: Session, contact_id: int, contact: ContactUpdate):
+    db_contact = db.query(Contact).filter(Contact.id == contact_id).first()
+    if not db_contact:
+        return None
+    for key, value in contact.dict().items():
+        setattr(db_contact, key, value)
+    db.commit()
+    db.refresh(db_contact)
+    return db_contact
+
+def delete_contact(db: Session, contact_id: int):
+    db_contact = db.query(Contact)\
+        .filter(Contact.id == contact_id).first()
+    if db_contact:
+        db.delete(db_contact)
+        db.commit()
+    return db_contact
+
+def search_contacts(db: Session, query: str):
+    return db.query(Contact).filter(
+        (Contact.first_name.contains(query)) |
+        (Contact.last_name.contains(query)) |
+        (Contact.email.contains(query))
+    ).all()
+
+def get_upcoming_birthdays(db: Session):
+    today = date.today()
+    next_week = today + timedelta(days=7)
+    return db.query(Contact)\
+        .filter(Contact.birthday.between(today, next_week)).all()
